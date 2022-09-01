89ffaaee763baa98e0edf65dbae0540b3334a415
apastrana-auntap <apastrana@auntap.com>
2022-08-31 21:12:05 -0300

    Use backend service

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -6,6 +6,11 @@
 // const defaultLocale = "es-MX";
 
 // const isDev = process.env.NODE_ENV === "development";
+const LocizeBackend = require("i18next-locize-backend/cjs");
+const ChainedBackend = require("i18next-chained-backend").default;
+const LocalStorageBackend = require("i18next-localstorage-backend").default;
+
+const isBrowser = typeof window !== "undefined";
 
 module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
@@ -14,6 +19,20 @@ module.exports = {
     defaultLocale: "en",
     locales: ["en", "es"],
   },
+  backend: {
+    backendOptions: [
+      {
+        expirationTime: 60 * 60 * 1000, // 1 hour
+      },
+      {
+        projectId: "d3b405cf-2532-46ae-adb8-99e88d876733",
+        version: "latest",
+      },
+    ],
+    backends: isBrowser ? [LocalStorageBackend, LocizeBackend] : [],
+  },
+  serializeConfig: false,
+  use: isBrowser ? [ChainedBackend] : [],
   localeExtension: "js",
   react: { useSuspense: false },
   // localePath,
